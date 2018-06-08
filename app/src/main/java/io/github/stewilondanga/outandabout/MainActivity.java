42a578fe3cea22c2e0de8516fdb3218537b6abe1
Stewart ilondanga <stilondanga@gmail.com>
2018-06-08 18:36:43 +0300

    add app/build.gradle

--- app/src/main/java/io/github/stewilondanga/outandabout/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/outandabout/MainActivity.java
@@ -1,16 +1,19 @@
 package io.github.stewilondanga.outandabout;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindExploreButton;
     private Button mFindAboutButton;
     private EditText mLocationEditText;
+    private TextView mMainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +22,19 @@ public class MainActivity extends AppCompatActivity {
         mFindExploreButton = (Button) findViewById(R.id.mainbutton1);
         mFindAboutButton = (Button) findViewById(R.id.mainbutton2);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mMainTextView = (TextView) findViewById(R.id.mainTextView);
 
         mFindExploreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, EventActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
+
+                @Override
+                public void onClick(View v) {
+                    String location = mLocationEditText.getText().toString();
+                    Intent intent = new Intent(MainActivity.this, EventActivity.class);
+                    intent.putExtra("location", location);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -37,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Typeface CaviarDreams = Typeface.createFromAsset(getAssets(), "fonts/CaviarDreams.ttf");
+        mMainTextView.setTypeface(CaviarDreams);
     }
 
 }
