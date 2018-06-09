749a4df142c60e83fe8bcf5841639c3a35798d7a
Stewart ilondanga <stilondanga@gmail.com>
2018-06-09 20:22:52 +0300

    add app/src/main/java/io/github/stewilondanga/outandabout/EventActivity.java

--- app/src/main/java/io/github/stewilondanga/outandabout/EventActivity.java
+++ app/src/main/java/io/github/stewilondanga/outandabout/EventActivity.java
@@ -8,33 +8,34 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView;
 import android.view.View;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class EventActivity extends AppCompatActivity {
-    public TextView mLocationTextView;
-    private ListView mListView;
-    private String[] location = new String[] {"Nairobi", "Kampala", "Dodoma", "Kigali"};
+    @BindView(R.id.eventTextView) TextView mLocationTextView;
+    @BindView(R.id.eventListView) ListView mListView;
+
+    private String[] Places = new String[] {"Nairobi", "Kampala", "Dodoma", "Kigali"};
     private String[] event = new String[] {"Concert", "Festivity", "Rally", "Wedding"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        Intent intent = getIntent();
-        final String location = intent.getStringExtra("location");
-        mLocationTextView.setText(location);
+        ButterKnife.bind(this);
 
         mListView = (ListView) findViewById(R.id.eventListView);
         mLocationTextView = (TextView) findViewById(R.id.eventTextView);
 
-        OutAndAboutArrayAdapter adapter = new OutAndAboutArrayAdapter(this, android.R.layout.simple_list_item_1, location, event);
+        OutAndAboutArrayAdapter adapter = new OutAndAboutArrayAdapter(this, android.R.layout.simple_list_item_1, Places, event);
         mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int I, long l) {
                 String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(EventActivity.this, location, Toast.LENGTH_SHORT).show();
+                Toast.makeText(EventActivity.this, "location", Toast.LENGTH_SHORT).show();
             }
         });
 
