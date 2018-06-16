b7c8bfd1370a11b8410f70f43bab19ab3c3c1a8e
Stewart ilondanga <stilondanga@gmail.com>
2018-06-16 22:06:52 +0300

    add app/src/main/java/io/github/stewilondanga/outandabout/Constants.java

--- app/src/main/java/io/github/stewilondanga/outandabout/Constants.java
+++ app/src/main/java/io/github/stewilondanga/outandabout/Constants.java
@@ -5,5 +5,7 @@ package io.github.stewilondanga.outandabout;
  */
 
 public class Constants {
-    public static final String API_KEY = BuildConfig.API_KEY
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String MEETUP_BASE_URL = "https://api.meetup.com/find/upcoming_events?";
+    public static final String MEETUP_QUERY_PARAMETER = "event";
 }
