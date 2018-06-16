89e7361a0b5e0a88f9cf3a55fc83f302e44fdcf7
Stewart ilondanga <stilondanga@gmail.com>
2018-06-16 21:13:04 +0300

    add app/src/main/java/io/github/stewilondanga/outandabout/Constants.java

---
+++ app/src/main/java/io/github/stewilondanga/outandabout/Constants.java
@@ -0,0 +1,9 @@
+package io.github.stewilondanga.outandabout;
+
+/**
+ * Created by stewart on 6/16/18.
+ */
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY
+}
