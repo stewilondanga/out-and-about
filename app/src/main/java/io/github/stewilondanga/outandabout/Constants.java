a160d80d51032498344b400fd40e5d295c1ba079
Stewart ilondanga <stilondanga@gmail.com>
2018-06-28 10:22:13 +0300

    add app/src/main/java/io/github/stewilondanga/outandabout/Constants.java

--- app/src/main/java/io/github/stewilondanga/outandabout/Constants.java
+++ app/src/main/java/io/github/stewilondanga/outandabout/Constants.java
@@ -10,4 +10,5 @@ public class Constants {
     public static final String API_QUERY_PARAMETER = "event_list_item";
     public static final String LATITUDE_QUERY_PARAMETER = "lat";
     public static final String LONGITUDE_QUERY_PARAMETER = "lon";
+    public static final String FIREBASE_CHILD_SEARCHED_LISTITEM = "searchedListitem";
 }
